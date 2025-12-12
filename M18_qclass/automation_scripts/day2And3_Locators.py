9b9ac5bc85be80ae3e62e4e7877b83568da88e55
pvsskrishna <saikrishna534350@gmail.com>
2025-12-12 16:31:41 +0530

    Day2 and Day3 topics related to HTML and Introduction to Locators.

---
+++ M18_qclass/automation_scripts/day2And3_Locators.py
@@ -0,0 +1,428 @@
+"""
+launching empty browser:
+------------------------
+*to launch empty browser developer has written code inside a browser specific class
+constructor.
+*constructor will execute when ever we create an object
+*each browser specific class should be import from below statement.
+    from selenium.webdriver import BrowserClassName
+"""
+from time import sleep
+from webbrowser import Chrome
+
+from selenium.webdriver.common.by import By
+
+"""
+class Chrome:
+    def __init__(self):
+        #code to launch empty
+        chrome browser
+
+c = Chrome()            #object creation
+"""
+#ws to launch empty chrome browser
+# from selenium.webdriver import Chrome
+# c = Chrome()
+
+#ws to launch empty firefox browser
+# from selenium.webdriver import Firefox
+# f = Firefox()
+
+#ws to launch empty edge  browser
+# from selenium.webdriver import Edge
+# e = Edge()
+###########################################################################################3
+#program to with stand a chrome browser for long duration
+"""
+*as per the latest version of selenium chrome browser will close automatically, if we
+want to with stand a browser for long duration then we should follow the below code.
+"""
+"""
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+
+o = ChromeOptions()
+o.add_experimental_option("detach", True)
+
+driver = Chrome(options=o)
+"""
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
+
+o = ChromeOptions()
+o.add_experimental_option("detach", True)
+##################################################################################################################
+#10/12/2025
+###############################################################################################
+#get(): it is used to enter the URL
+#       it will accept both secured(https) and non-secured(http) URL.
+#       if we pass other than secured and non-secured URL then it will throw, InvalidArgumentException
+#   syntax: driver.get("URL")
+
+#example on secured URL
+"""
+driver = Chrome(options=o)
+driver.get("https://www.fb.com")
+"""
+#example on non-secured URL
+"""
+driver = Chrome(options=o)
+driver.get("http://www.fb.com")
+"""
+#example on invalid URL
+"""
+driver = Chrome(options=o)
+driver.get("www.fb.com")
+#InvalidArgumentException
+"""
+################################################################################################
+#close():it will close the current window/tab.
+#   syntax:driver.close()
+
+#quit():it will close complete browser.
+#   syntax:driver.quit()
+
+#example on close() method
+"""
+driver = Chrome(options=o)
+driver.get("https://www.instagram.com/")
+sleep(1)
+driver.close()
+"""
+#example on quit() method
+"""
+driver = Chrome(options=o)
+driver.get("https://www.instagram.com/")
+sleep(1)
+driver.quit()
+"""
+###############################################################################################
+# maximize_window(): it is used to maximize the window.
+#   syntax: driver.maximize_window()
+# minimize_window(): it is used to minimize the window.
+#   syntax: driver.minimize_window()
+# fullscreen_window(): it is used to set full screen window.
+#   syntax: driver.fullscreen_window()
+
+#ws to maximize, minimize and full screen the window.
+"""
+driver = Chrome(options=o)
+driver.get("https://www.instagram.com/")
+sleep(1)
+driver.maximize_window()
+sleep(1)
+driver.minimize_window()
+sleep(1)
+driver.maximize_window()
+sleep(1)
+driver.fullscreen_window()
+"""
+#########################################################################################
+#back(): it used to click on backward arrow in the browser.
+#   syntax: driver.back()
+#forward(): it used to click on forward arrow in the browser.
+#   syntax: driver.forward()
+#refresh(): it used to cick on refresh icon in the browser.
+#   syntax: driver.refresh()
+
+#ws to perform backward, forward, refresh action in the browser.
+"""
+driver = Chrome(options=o)
+driver.get("https://www.flipkart.com")
+driver.maximize_window()
+sleep(2)
+driver.back()
+sleep(2)
+driver.forward()
+sleep(2)
+driver.refresh()
+"""
+################################################################################################
+#get_window_size(): it will return dictionary of height and width of a browser.
+#   syntax: driver.get_window_size()
+#get_window_position(): it will return dictionary of x and y axis of a browser.
+#   syntax: driver.get_window_position()
+#get_window_rect(): it will return dictionary of height, width, x and y axis of a browser.
+#   syntax: driver.get_window_rect()
+
+#ws to print x, y, width, height a of browser
+"""
+driver = Chrome(options=o)
+driver.get("https://www.ajio.com")
+driver.maximize_window()
+data = driver.get_window_size()
+print(data)     #{'width': 1382, 'height': 744}
+data1 = driver.get_window_position()
+print(data1)    #{'x': -8, 'y': -8}
+data2 = driver.get_window_rect()
+print(data2)    #{'height': 744, 'width': 1382, 'x': -8, 'y': -8}
+"""
+###################################################################################################
+#set_window_size(): it is used to set the window based on height and width.
+#   syntax: driver.set_window(width, height)
+#set_window_position(): it is used to set the window based on x and y axis.
+#   syntax: driver.set_window(x, y)
+#set_window_rect(): it is used to set the window base on x, y, height and width.
+#   syntax: driver.set_window(x, y, width, height)
+
+#ws to set the window based on x, y, width, height a of browser
+"""
+driver = Chrome(options=o)
+driver.get("https://www.fb.com")
+driver.maximize_window()
+sleep(2)
+driver.set_window_size(100, 150)
+sleep(2)
+driver.set_window_position(67, 23)
+sleep(2)
+driver.set_window_rect(290, 87, 234, 500)
+"""
+
+##############################################################################################
+"""
+verification property
+"""
+#title: it will return current title of a webpage.
+#   syntax: driver.title
+#ws to print title of nike.com webpage
+"""
+driver = Chrome(options=o)
+driver.get("https://www.nike.com/in/")
+driver.maximize_window()
+print(driver.title)         #Nike. Just Do It. Nike IN
+sleep(2)
+print(driver.title)         #Men's Shoes, Clothing & Accessories. Nike IN
+"""
+#current_url: it will return current URL of a webpage.
+#   syntax: driver.current_url
+#ws to print url of a webpage
+"""
+driver = Chrome(options=o)
+driver.get("https://www.nike.com/in/")
+driver.maximize_window()
+print(driver.current_url)         #https://www.nike.com/in/
+sleep(3)
+print(driver.current_url)         #https://www.nike.com/in/women
+"""
+#page_source: it will return current webpage html source code
+#   syntax: driver.page_source
+#ws to print current webpage source code
+"""
+driver = Chrome(options=o)
+driver.get("https://www.nike.com/in/")
+driver.maximize_window()
+print(driver.page_source)
+#<html> ... </html>
+"""
+###############################################################################################
+#11/12/2025
+"""
+html:
+-----
+*html stands for hyper text markup language.
+*html is used to design and develop the webpages.
+*a webpage will be developed by html tags.
+*a keyword which is enclosed within angular brackets(<keyword>) is called as tags.
+*if we open any tag it is mandatory to close the tag(</keyword>).
+
+general structure of html:
+==========================
+<html>
+    <head>
+        <title> title of the webpage </title>
+    </head>
+    <body>
+        #all the components
+    </body>
+</html>
+
+steps to create a webpage:
+==========================
+step1: open notepad
+step2: type the html code and save with .html extension
+step3: select save as type and "all files" option
+"""
+"""
+<html>
+          <head>	
+                <title>E20 Selenium Batch</title>
+          </head>
+          <body bgcolor="pink">		
+         	Username:<input type="text" id="a1"><br/>	
+            Password:<input type="password" id="a2"><br/>
+            <input type="radio" name="n1">New User
+            <input type="radio" name="n2">Old User<br/>
+            <input type="checkbox" class="c1">I accept T&C**<br/>
+            <a href="https://www.gmail.com" class="c2">Forgotten Passwod</a><br/>
+            <img src="C:\\Users\\Hp\\Downloads\\selenium-image.png"><br/>
+            <input type="submit" value="Submit">
+            <input type="reset" value="Cancel">
+         </body>	
+</html>
+"""
+#########################################################################################################
+"""
+locators:
+---------
+*finding/inspecting/searching/locating the path of an element in a webpage is called
+as locators.
+*all locators are belongs to "By" clas
+
+why locators?
+-------------
+*before performing any action(entering, clicking, selecting,..) in a webpage first we 
+need to find the path of an element in a webpage because selenium is not a human it 
+doesn't know the path of element in a webpage. 
+
+types of locators:
+------------------
+1.id            5.link text
+2.name          6.partial link text
+3.class name    7.css selector
+4.tag name      8.xpath
+
+*to find an element in a webpage there are 2 methods are present,
+1.find_element()                2.find_elements()
+
+find_element():
+---------------
+*it is used to find single element in a webpage.
+*the return type of find_element() is web element.
+*if locator value matches with multiple element then it will return 1st element address.
+*if the locator value not matches with any element then it will return "NoSuchElementException".
+syntax: driver.find_element(locator_name, "locator_value")
+                                |
+                                |_By.ID, By.NAME, ...
+find_elements():
+----------------
+*it is used to find multiple elements in a webpage.
+"""
+
+#1.id
+#possiablity1: locator value matches with exactly 1 element
+"""
+driver = Chrome(options=o)
+driver.get("file:///C:/Users/Hp/Desktop/demo.html")
+driver.maximize_window()
+#way1: find the element and storing address in a variable -> webelement
+username = driver.find_element(By.ID, 'a1')
+username.send_keys("selenium@gmail.com")
+                    (or)
+#way2: find the element and performing action on same line
+driver.find_element(By.ID, 'a1').send_keys("selenium@gmail.com")
+"""
+#possiablity2: if locator matches with multiple elements then it will return 1st element address
+"""
+driver = Chrome(options=o)
+driver.get("file:///C:/Users/Hp/Desktop/demo.html")
+driver.maximize_window()
+driver.find_element(By.ID, "a1").send_keys("selenium@123")
+"""
+#possiablity3: locator values doesn't match with any element then it will throw "NoSuchElementException"
+"""
+driver = Chrome(options=o)
+driver.get("file:///C:/Users/Hp/Desktop/demo.html")
+driver.maximize_window()
+driver.find_element(By.ID, "a10").send_keys("selenium@123")
+#NoSuchElementException
+"""
+#########################################################################################################
+#12/12/2025
+#ws to login into facebook.com
+"""
+driver = Chrome(options=o)
+driver.get("https://www.facebook.com/")
+driver.maximize_window()
+driver.find_element(By.ID, 'email').send_keys('selenium@gmail.com')
+driver.find_element(By.ID, 'pass').send_keys('selenium@123')
+driver.find_element(By.NAME, 'login').click()
+"""
+##############################################################################################
+#2.name
+#ws to search for python selenium videos in youtube.com
+"""
+driver = Chrome(options=o)
+driver.get("https://www.youtube.com/")
+driver.maximize_window()
+driver.find_element(By.NAME, "search_query").send_keys("python selenium")
+driver.find_element(By.CLASS_NAME, "ytSearchboxComponentSearchButton").click()
+"""
+##############################################################################################
+#3.class name
+#ws to search for shirts in myntra.com
+"""
+driver = Chrome(options=o)
+driver.get("https://www.myntra.com/")
+driver.maximize_window()
+driver.find_element(By.CLASS_NAME, "desktop-searchBar").send_keys("shirts")
+driver.find_element(By.CLASS_NAME, "desktop-submit").click()
+"""
+#ws to search for movie in book my show
+"""
+driver = Chrome(options=o)
+driver.get("https://in.bookmyshow.com/explore/home/bengaluru")
+driver.maximize_window()
+driver.find_element(By.CLASS_NAME, "sc-1or3vea-15.bMjnfo").click()
+driver.find_element(By.CLASS_NAME, "sc-vuznvr-5.extnng").send_keys("Kantara")
+"""
+#ws to search for shirts in amazon
+"""
+driver = Chrome(options=o)
+driver.get("https://www.amazon.in/ref=nav_logo")
+driver.maximize_window()
+driver.find_element(By.CLASS_NAME, "nav-input.nav-progressive-attribute").send_keys("shirts")
+driver.find_element(By.ID, "nav-search-submit-button").click()
+"""
+##############################################################################################
+#4.tag name
+#ws to enter password in password text field
+"""
+driver = Chrome(options=o)
+driver.get("file:///C:/Users/Hp/Desktop/demo.html")
+driver.maximize_window()
+driver.find_element(By.TAG_NAME, 'input').send_keys('selenium@123')
+"""
+
+"""
+way to inspect html code
+========================
+*Fn+F12 (or) F12
+*ctrl+shift+i
+*click on 3 dots on(:) top right corner --> click on more tools --> click on developer tools
+"""
+"""
+sample html code:
+=================
+*a html code will have 3 component,
+1.tag name: anything which is present after angular brackets(<tag_name)    
+2.attribute: LHS=RHS (LHS --> attribute name, RHS --> attribute value)     
+3.text: anything which is present before closing tag
+
+         attribute_name  attribute_value
+             /              /
+<  a        href="https://www.gmail.com"     id="a1"     name="n1" >  Gmail </a>
+   |                 |                          |           |           \
+1.tag name      2.attribute1                    attribute2  attribute3   3.text
+"""
+"""
+assignment:
+-----------
+launch --> https://demowebshop.tricentis.com/ --> click on register link --> enter values for 
+all the fields and register --> click on logout link --> click on login link --> enter un&pwd 
+login  
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
